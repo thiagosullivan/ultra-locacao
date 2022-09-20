7fce3ccca1e6469e121f9f34abf568b94992ea39
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-09-20 05:09:41 -0300

    About section

---
+++ components/About/index.js
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import React from 'react';
+import ImageOne from '../../assets/about-img-1.jpg';
+import ImageTwo from '../../assets/about-img-2.jpg';
+import { AboutUsContainer } from './style';
+
+function AboutUs() {
+  return (
+    <AboutUsContainer>
+        <div className='about__content'>
+            <h2>Allow Planet <br />
+            <span>the Future</span></h2>
+            <div className='about__div1'>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+            <div className='about__div2'>
+                <div className='about__img1'>
+                    <Image src={ImageOne} />
+                </div>
+                <div className='about__img2'>
+                    <Image src={ImageTwo} />
+                </div>
+            </div>
+            <div className='about__div3'>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+        </div>
+
+    </AboutUsContainer>
+  )
+}
+
+export default AboutUs
---
+++ components/About/style.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const AboutUsContainer = styled.section`
+
+    .about__content {
+        max-width: 1300px;
+        width: 100%;
+        margin: 0 auto;
+        position: relative;
+        padding: 9.3rem 1rem;
+        
+        h2 {
+            font-family: 'Kanit', sans-serif;
+            font-size: 7.5rem;
+            line-height: 0.7;
+            color: ${({theme}) => theme.secondary};
+            text-shadow: 0px 5px 0px rgba(0, 0, 0, 0.4);
+            font-weight: bold;
+            text-align: right;
+            position: absolute;
+            z-index: 1;
+
+            span {
+                font-family: 'Kanit', sans-serif;
+                font-size: 6.25rem;
+                line-height: 0.7;
+                font-weight: 400;
+            }
+        }
+
+        p {
+            font-family: 'Kanit', sans-serif;
+            color: ${({theme}) => theme.third};
+        }
+
+        .about__div1 {
+            max-width: 475px;
+            width: 100%;
+            margin-left: auto;
+
+            p {
+                text-align: justify;
+            }
+        }
+
+        .about__div2 {
+            display: flex;
+            justify-content: flex-end;
+
+            .about__img1 {
+                margin-right: 1.87rem;
+            }
+        }
+
+        .about__div3 {
+            max-width: 1130px;
+            width: 100%;
+            margin: 3.75rem auto 0;
+
+            p {
+                text-align: justify;
+            }
+        }
+    }
+`
