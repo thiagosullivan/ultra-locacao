566ddf8335e013f13f137e4577387d4f1501edde
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-09-21 06:02:11 -0300

    Changes for deploy #2

--- components/Testimonial/index.js
+++ components/Testimonial/index.js
@@ -17,14 +17,14 @@ function Testimonial() {
         <div className='testimonial__slider'>
             <Carousel interval={3000} activeIndex={index} onSelect={handleSelect} controls={true} variant="dark">
                 <Carousel.Item>
-                    <p>"Vivamus aliquet felis eu diam ultricies congue. Morbi porta lorem nec consectetur porta. Sed quis dui elit. Pellentesque habitant morbi tristique senectus et netus et male Sed vestibulum orci"</p>
+                    <p>&ldquo;Vivamus aliquet felis eu diam ultricies congue. Morbi porta lorem nec consectetur porta. Sed quis dui elit. Pellentesque habitant morbi tristique senectus et netus et male Sed vestibulum orci&ldquo;</p>
                     <div className='autor__testimonial'>
                         <img src="https://i.picsum.photos/id/182/60/60.jpg?hmac=PC6Ff1xqU7q_DqJyOofEEiG3mwFGMF5Lf6RfMUXdiJI" />
                         <p>Graham Cracker, <span>Designer at Colorlib</span></p>
                     </div>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <p>"Vivamus aliquet felis eu diam ultricies congue. Morbi porta lorem nec consectetur porta. Sed quis dui elit. Pellentesque habitant morbi tristique senectus et netus et male Sed vestibulum orci"</p>
+                    <p>&ldquo;Vivamus aliquet felis eu diam ultricies congue. Morbi porta lorem nec consectetur porta. Sed quis dui elit. Pellentesque habitant morbi tristique senectus et netus et male Sed vestibulum orci&ldquo;</p>
                     <div className='autor__testimonial'>
                         <img src="https://i.picsum.photos/id/182/60/60.jpg?hmac=PC6Ff1xqU7q_DqJyOofEEiG3mwFGMF5Lf6RfMUXdiJI" />
                         <p>Graham Cracker, <span>Designer at Colorlib</span></p>
